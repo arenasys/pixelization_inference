fdcd5d28b9b5cb1b264ebf3f5a85edf6e5d842d5
arenasys <arenatemp@mailfence.com>
2025-03-12 13:46:41 +0800

    add server

---
+++ server.py
@@ -0,0 +1,60 @@
+import os
+import http.server
+import uploadserver
+import shutil
+
+working_dir = 'working'
+unprocessed_dir = os.path.join(working_dir, 'unprocessed')
+processed_dir = os.path.join(working_dir, 'processed')
+outputs_dir = os.path.join(working_dir, 'outputs')
+
+os.makedirs(working_dir, exist_ok=True)
+os.makedirs(unprocessed_dir, exist_ok=True)
+os.makedirs(processed_dir, exist_ok=True)
+os.makedirs(outputs_dir, exist_ok=True)
+
+def handle_uploads():
+  for file in os.listdir(working_dir):
+    old_file = os.path.join(working_dir, file)
+    if not os.path.isfile(old_file): continue
+    new_file = os.path.join(unprocessed_dir, file)
+    os.rename(old_file, new_file)
+  
+  import subprocess
+  subprocess.run(['python', 'pixelization.py', '--input', 'unprocessed', '--output', 'outputs'])
+  
+  # debug
+  for file in os.listdir(unprocessed_dir):
+    old_file = os.path.join(unprocessed_dir, file)
+    new_file = os.path.join(outputs_dir, file)
+    shutil.copy(old_file, new_file)
+    
+  for file in os.listdir(unprocessed_dir):
+    old_file = os.path.join(unprocessed_dir, file)
+    new_file = os.path.join(processed_dir, file)
+    os.rename(old_file, new_file)
+
+class Args:
+  port = 8000
+  cgi = False
+  allow_replace = False
+  bind = None
+  directory = working_dir
+  theme = 'dark'
+  server_certificate = None
+  client_certificate = None
+  basic_auth = None
+  basic_auth_upload = None
+uploadserver.args = Args()
+
+old_receive_upload = uploadserver.receive_upload
+def new_receive_upload(handler: http.server.BaseHTTPRequestHandler):
+  status, message = old_receive_upload(handler)
+  
+  if status != http.HTTPStatus.BAD_REQUEST:
+    handle_uploads()
+  
+  return status, message
+uploadserver.receive_upload = new_receive_upload
+
+uploadserver.serve_forever()
